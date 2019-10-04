80445f52a94f0080494d77e6ce0961eaa2c63f19
BuildTools <unconfigured@null.spigotmc.org>
2019-10-04 18:39:08 +0200

    Changement 1.0

---
+++ TP1 - code.R
@@ -0,0 +1,14 @@
+#Enregistrement d'une variable contenant le jeu de données
+
+#myData <- read.table(file="online_shoppers_intention.csv", header=TRUE, sep=",")
+
+probabilityDistribution <- function(data, attributName)  {
+  data[,names(data)[0]]
+}
+
+
+
+runAnalysis <- function(fileData, indexContVar, classID) {
+  myData <- read.table(file=fileData, header=TRUE, sep=",")
+  
+}
