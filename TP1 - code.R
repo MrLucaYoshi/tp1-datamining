6651afba4bbd3684f5af574698676909e770883e
Chad <chadi-aebi@hotmail.com>
2019-10-05 00:05:45 +0200

    changement 2.0

--- TP1 - code.R
+++ TP1 - code.R
@@ -1,16 +1,44 @@
 #Enregistrement d'une variable contenant le jeu de données
 
 #myData <- read.table(file="online_shoppers_intention.csv", header=TRUE, sep=",")
+################################
+#Input exemples:
+#discretes<- c(1,3,5,10,11,12,13,14,15,16,17)
+#attToRemove<- c(12,13)
+#classId<-18 #class = target ???
+#runAnalysis(myData,discretes,attToRemove,classId)
+#
 
-probabilityDistribution <- function(data, attributName)  {
-  data[,names(data)[0]]
+probabilityDistribution <- function(data, attribute)  {
+  barplot(prop.table(table(data[,attribute])),main = names(data)[attribute]) 
 }
 
 
-
-runAnalysis <- function(fileData, indexContVar, classID) {
-  myData <- read.table(file=fileData, header=TRUE, sep=",")
+runAnalysis <- function(fileName, discretes, attToRemove, classId) {
+  data <- read.table(file=fileName, header=TRUE, sep=",")
+  
+  for(d in c(1:length(data))){
+    if(is.element(d,discretes)){
+      #Discretes
+      #P(att)
+      windows()
+      probabilityDistribution(data,d)
+      contTable<-table(data[,d],data[,classId])
+      #P(target|att)
+      windows()
+      barplot(prop.table(contTable),1)
+    }else{
+      #Continuous
+      #
+      mean(data[,d])
+      var(data[,d])
+      #(target|att)
+      
+    }
+  }
+  
   
 }
 
-#barplot(prop.table(table(shop[,11])))
+
+#c(1:length(shop))-discretes
