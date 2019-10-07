4d9d6129a8a53bf7e971ed410e04e5ec8387d58d
Chad <chadi-aebi@hotmail.com>
2019-10-07 19:03:36 +0200

    :D

--- TP1 - code.R
+++ TP1 - code.R
@@ -12,10 +12,19 @@
 probabilityDistribution <- function(data, attribute)  {
   barplot(prop.table(table(data[,attribute])),main = names(data)[attribute]) 
 }
-
+colVars <- function(x, na.rm=FALSE, dims=1, unbiased=TRUE, SumSquares=FALSE,
+                    twopass=FALSE) {
+  if (SumSquares) return(colSums(x^2, na.rm, dims))
+  N <- colSums(!is.na(x), FALSE, dims)
+  Nm1 <- if (unbiased) N-1 else N
+  if (twopass) {x <- if (dims==length(dim(x))) x - mean(x, na.rm=na.rm) else
+    sweep(x, (dims+1):length(dim(x)), colMeans(x,na.rm,dims))}
+  (colSums(x^2, na.rm, dims) - colSums(x, na.rm, dims)^2/N) / Nm1
+}
 
 runAnalysis <- function(fileName, discretes, attToRemove, classId) {
   data <- read.table(file=fileName, header=TRUE, sep=",")
+  hist(colMeans(data[,-c(attToRemove,discretes,classId)]))
   
   for(d in c(1:length(data))){
     if(!is.element(d,attToRemove)){
