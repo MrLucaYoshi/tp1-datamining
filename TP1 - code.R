dfeacdd082941d9c2d29e0a6401e89d98ee82a02
Chad <chadi-aebi@hotmail.com>
2019-10-13 22:56:07 +0200

    Version Finale TP1

--- TP1 - code.R
+++ TP1 - code.R
@@ -1,74 +1,82 @@
 #Enregistrement d'une variable contenant le jeu de données
 
-#myData <- read.table(file="online_shoppers_intention.csv", header=TRUE, sep=",")
+#mymyData <- read.table(file="online_shoppers_intention.csv", header=TRUE, sep=",")
 ################################
 #Input exemples:
 #discretes<- c(1,3,5,10,11,12,13,14,15,16,17)
 #attToRemove<- c(12,13)
 #classId<-18 #class = target ???
-#runAnalysis(myData,discretes,attToRemove,classId)
+#runAnalysis(mymyData,discretes,attToRemove,classId)
 #
 
-probabilityDistribution <- function(data, attribute)  {
-  barplot(prop.table(table(data[,attribute])),main = names(data)[attribute])
-}
-
-computeMeanContinue <- function(data, attribute) {
-  mean(data[,attribute])
-}
-
-computeVarContinue <- function(data, attribute) {
-  var(data[,attribute])
+if(.Platform$OS.type=="windows") {
+  quartz<-function() windows()
 }
 
 
-
-colVars <- function(x, na.rm=FALSE, dims=1, unbiased=TRUE, SumSquares=FALSE,
-                    twopass=FALSE) {
-  if (SumSquares) return(colSums(x^2, na.rm, dims))
-  N <- colSums(!is.na(x), FALSE, dims)
-  Nm1 <- if (unbiased) N-1 else N
-  if (twopass) {x <- if (dims==length(dim(x))) x - mean(x, na.rm=na.rm) else
-    sweep(x, (dims+1):length(dim(x)), colMeans(x,na.rm,dims))}
-  (colSums(x^2, na.rm, dims) - colSums(x, na.rm, dims)^2/N) / Nm1
+probabilityDistribution <- function(myData, attribute)  {
+  barplot(prop.table(table(myData[,attribute])),main = names(myData)[attribute])
 }
 
 
 runAnalysis <- function(fileName, discretes, attToRemove, classId) {
-  data <- read.table(file=fileName, header=TRUE, sep=",")
-  windows()
-  hist(colMeans(data[,-c(attToRemove,discretes,classId)]))
-  windows()
-  hist(colVars(data[,-c(attToRemove, discretes, classId)]))
+  myData <- read.table(file=fileName, header=TRUE, sep=",")
+  #quartz()
+  #hist(colMeans(myData[,-c(attToRemove,discretes,classId)]))
+  #quartz()
+  #hist(colVars(myData[,-c(attToRemove, discretes, classId)]))
+  print(paste("Valeur cible-> ", names(myData)[classId],sep=" pour "))
+  #Moyenne et variance des variables continues
+  print("Moyennes des valeurs continues")
+  print(colMeans(myData[, -discretes]))
+  print("Variances des valeurs continues")
+  print(diag(var(myData[, -discretes])))
   
+  #Moyenne et variance des variables continues, en fonction de la variable 
   
-  for(d in c(1:length(data))){
+  for(rowTarget in unique(myData[,classId])){
+    print(paste("Moyennes des valeurs continues, en fonction de la variable",rowTarget,sep=" "))
+    print(colMeans(myData[myData[,classId]==rowTarget, -discretes]))
+    print(paste("Variances des valeurs continues, en fonction de la variable",rowTarget,sep=" "))
+    print(diag(var(myData[myData[,classId]==rowTarget, -discretes])))
+    #Nuage points
+    quartz()
+    plot(myData[,8:9],col="green")
+    points(myData[myData[,classId]==rowTarget,8:9],col="red")
+  }
+
+  
+  for(d in c(1:length(myData))){
     if(!is.element(d,attToRemove)){
       if(is.element(d,discretes)){
         #Discretes
         #P(att)
-        windows()
-        probabilityDistribution(data,d)
-        contTable<-table(data[,d],data[,classId])
+        quartz()
+        probabilityDistribution(myData,d)
+        contTable<-table(myData[,d],myData[,classId])
         #P(target|att)
-        windows()
+        quartz()
         barplot(prop.table(contTable),
                 main="Distribution target",
                 col=c("blue","red","orange","yellow","purple","darkblue","black","white","pink","green"),
                 legend=TRUE,
                 xlab = "Revenue",
-                ylab = names(data)[d]
+                ylab = names(myData)[d]
                 )
                 
       }else{
         #Continuous
         #
-        print(names(data)[d])
-        windows()
-        print(computeMeanContinue((data)[d]))
-        print(computeVarContinue((data)[d]))
-        #(target|att)
-        
+        #print(names(myData)[d])
+        #print(paste("Moyenne :",mean(myData[,d]),sep = " "))
+        #print(paste("variance :",var(myData[,d]),sep = " "))
+        quartz()
+        hist(prop.table(table(myData[,d])), main=names(myData)[d])
+        #En fonction de target
+        for(rowTarget in unique(myData[,classId])){
+          quartz()
+          hist(t(prop.table(table(myData[myData[,classId]==rowTarget,][,c(d,classId)]))), main=paste(rowTarget,names(myData[d]),sep=" "),  breaks = seq(from=0, to=1, by=0.1))
+        }
       }
     }
   }
